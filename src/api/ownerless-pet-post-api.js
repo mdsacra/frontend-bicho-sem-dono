09efddb503054d7497ff62732b4d7b799d58b5e2
Matheus Sacramento <matheus.sacramento@qualyteam.com.br>
2022-09-29 21:02:17 -0300

    Create createOwnerlessPetPost api method

--- src/api/ownerless-pet-post-api.js
+++ src/api/ownerless-pet-post-api.js
@@ -1,6 +1,20 @@
 const DEFAULT_API_URI = process.env.REACT_APP_DEFAULT_API_URI;
 
 export const listOwnerlessPetPosts = () => 
-    fetch(`${DEFAULT_API_URI}/ownerless-pet-post`)
+    fetch(`${DEFAULT_API_URI}/ownerless-pet-post`, {
+      method: "GET"
+    })
       .then(res => res.json())
       .then(data => data);
+
+export const createOwnerlessPetPost = (body) =>
+    fetch(`${DEFAULT_API_URI}/ownerless-pet-post`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8"
+      },
+      body: JSON.stringify(body)
+    })
+    .then((response) => response.json()
+    .then(data => console.log(data)))
+    .catch((error) => console.log(error));
