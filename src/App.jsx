08c09fd170a45b62edab84a66df8ab1a3e6e99c1
Matheus Sacramento <matheus.sacramento@qualyteam.com.br>
2022-09-29 21:03:08 -0300

    Create chakra bsd theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { InitialPage } from './initial-page/InitialPage';
+import { BichoSemDonoTheme } from './theme/bicho-sem-dono-theme/BichoSemDonoTheme';
 
 const App = () => {
   return (
     <div className="App">
-      <ChakraProvider>
+      <ChakraProvider theme={BichoSemDonoTheme} >
         <InitialPage />
       </ChakraProvider>
     </div>
---
+++ src/theme/bicho-sem-dono-theme/BichoSemDonoTheme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+import { TextAreaStyles as TextArea } from "./styles/TextAreaStyles";
+import { InputStyles as Input } from "./styles/InputStyles";
+export const BichoSemDonoTheme = extendTheme({
+    components: {
+        TextArea,
+        Input
+    }
+});
+
