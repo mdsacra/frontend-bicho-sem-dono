b8ddf22484794b755cddfac1f33c1e924e4a9768
Matheus Sacramento <mdsacra@gmail.com>
2022-10-12 20:41:54 -0300

    Add bicho sem dono colors in theme

--- src/theme/bicho-sem-dono-theme/BichoSemDonoTheme.js
+++ src/theme/bicho-sem-dono-theme/BichoSemDonoTheme.js
@@ -1,7 +1,18 @@
 import { extendTheme } from "@chakra-ui/react";
 import { TextAreaStyles as TextArea } from "./styles/TextAreaStyles";
 import { InputStyles as Input } from "./styles/InputStyles";
+
 export const BichoSemDonoTheme = extendTheme({
+	colors: {
+		bsd: {
+			red: "#FF3936",
+			yellow: "#F2CC0C",
+			brown: "#A66E4E",
+			blue: "#344459",
+			gray: "#F0F0F2",
+		}
+	},
+
 	components: {
 		TextArea,
 		Input
