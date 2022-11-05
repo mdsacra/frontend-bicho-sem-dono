7e95663ec71153978ddc268f6b80af66886412b5
Matheus Sacramento <mdsacra@gmail.com>
2022-11-05 19:31:20 -0300

    Pass prop to OwnerlessPetPostForm

--- src/initial-page/main-menu/MainMenu.jsx
+++ src/initial-page/main-menu/MainMenu.jsx
@@ -2,30 +2,36 @@ import React, { useState } from "react";
 import "./styles.css";
 import AddPostBtnImg from "../../assets/add_post_btn.png";
 import { OwnerlessPetPostForm } from "../post-forms/ownerless-pet-post-form/OwnerlessPetPostForm";
+import PropTypes from "prop-types";
 
-function MainMenu() {
+const MainMenu = ({ setPosts }) => {
 	const [isOwnerlessPetPostFormOpen, setIsOwnerlessPetPostFormOpen] = useState(false);
 
-	const handleForm = () => (setIsOwnerlessPetPostFormOpen(!isOwnerlessPetPostFormOpen));
+	const handleForm = () => {
+		setIsOwnerlessPetPostFormOpen(!isOwnerlessPetPostFormOpen);
+	};
 
 	return (
 		<>
 			{isOwnerlessPetPostFormOpen
-                && <OwnerlessPetPostForm onClose={handleForm} />}
+                && <OwnerlessPetPostForm onClose={handleForm} setPosts={setPosts} />}
+			
 			<div className="menu-bar" />
 			<div className="main-menu-container">
-				
-							
 				<div className="post-ownerless-pet">
 					<button onClick={() => handleForm()}>
 						<img src={AddPostBtnImg} alt="add_post_btn" />
 					</button>
 				</div>
-
 			</div>
 		</>
 		
 	);
-}
+};
 
 export { MainMenu };
+
+MainMenu.propTypes = {
+	posts: PropTypes.arrayOf(PropTypes.object),
+	setPosts: PropTypes.func
+};
