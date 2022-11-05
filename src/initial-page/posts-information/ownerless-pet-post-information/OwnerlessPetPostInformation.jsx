8588b98c6a24b08f0b019f2dc7cbce90cf48ec02
Matheus Sacramento <mdsacra@gmail.com>
2022-11-05 19:35:49 -0300

    Refactor OwnerlessPetPostInformation drawer

--- src/initial-page/posts-information/ownerless-pet-post-information/OwnerlessPetPostInformation.jsx
+++ src/initial-page/posts-information/ownerless-pet-post-information/OwnerlessPetPostInformation.jsx
@@ -1,47 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 import { Text } from "@chakra-ui/react";
 import { GhostButton } from "../../../common-components/buttons/GhostButton";
 import { CloseIcon } from "@chakra-ui/icons";
 import { VerticalSpace } from "../../../common-components/VerticalSpace";
 import { Drawer } from "../../../common-components/drawer/Drawer";
-import { IconVectorLayer } from "../../map/layers/icon-vector-layer/IconVectorLayer";
+import PropTypes from "prop-types";
 
-const OwnerlessPetPostInformation = () => {
-	const [postIconInformation, setPostIconInformation] = useState({});
-	const [isShowingPostInformation, setIsShowingPostInformation] = useState(false);
-    
-	IconVectorLayer((values) => {
-		setPostIconInformation(values);
-		setIsShowingPostInformation(true);
-	});
-    
-	return (
-		<>
-			{
-				isShowingPostInformation &&
-			<Drawer>
-				<div className="post-information-header">
-					<Text fontSize="2xl" color="bsd.blue">Pet abandonado</Text>
-					<Text 
-						fontSize="sm" 
-						fontWeight={400}
-						color="bsd.blue"
-					>
-						{postIconInformation.localization.address}
-					</Text>
+const OwnerlessPetPostInformation = ({ postInformation, onClose }) => (
+	<>
+		<Drawer>
+			<div className="post-information-header">
+				<Text fontSize="2xl" color="bsd.blue">Pet abandonado</Text>
+				<Text 
+					fontSize="sm" 
+					fontWeight={400}
+					color="bsd.blue"
+				>
+					{postInformation.localization.address}
+				</Text>
+			</div>
+			<div className="post-information-body">
+				<Text fontSize="xl" color="bsd.blue">{postInformation.description}</Text>
+				<VerticalSpace />
+				<div className="post-information-close-button">
+					<GhostButton textColor="bsd.blue" icon={<CloseIcon />} onClick={onClose} />
 				</div>
-				<div className="post-information-body">
-					<Text fontSize="xl" color="bsd.blue">{postIconInformation.description}</Text>
-					<VerticalSpace />
-					<div className="post-information-close-button">
-						<GhostButton textColor="bsd.blue" icon={<CloseIcon />} onClick={() => setIsShowingPostInformation(false)} />
-					</div>
-				</div>
-			</Drawer>
-			}
-		</>
-	);
-};
+			</div>
+		</Drawer>
+	</>
+);
 
 export { OwnerlessPetPostInformation };
+
+OwnerlessPetPostInformation.propTypes = {
+	postInformation: PropTypes.object.isRequired,
+	onClose: PropTypes.func.isRequired
+};
